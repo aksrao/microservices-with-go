5379dd4303d0318d1992e6b6f783d94d8621fa2b
aksrao1998 <aksrao1998@gmail.com>
2022-11-15 22:51:22 +0900

    day1

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write([]byte("error"))
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
 	http.ListenAndServe("127.0.0.1:8081", nil)
 }
